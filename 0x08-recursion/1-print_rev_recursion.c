eee7d86b807645cb442fbcf7ec3a9e6493b51467
GithinjiJustus <justusgithinji257@gmail.com>
2023-10-26 01:43:12 -0700

    finally was able to comlete

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,14 +4,11 @@
  * _puts_recursion - Prints a string followed by a new line
  * @s: The string to be printed
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
 		return;
-	}
 
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_puts_recursion(s + 1);
 }
