53973494b5813c0ea564b51a1392e805ea884182
GithinjiJustus <justusgithinji257@gmail.com>
2023-10-30 04:39:24 -0700

    its not bragging if y

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid function.
+ * free_grid - frees a 2 dimensional grid previously
+ * created by alloc_grid function.
  * @grid: the address of the two-dimensional grid
  * @height: height of the grid
  *
