80caff453cc5475d6b2d13cc847834ae326897a1
GithinjiJustus <justusgithinji257@gmail.com>
2023-11-27 12:16:22 -0800

    drive slo homie

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,27 +12,23 @@
  *
  * Return: actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t lenr, lenw;
 	char *buffer;
 
-	if (!filename = NULL)
+	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	lenr = read(fd, buffer, letters);
 	close(fd);
 	if (lenr == -1)
@@ -40,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
-
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
 	if (lenr != lenw)
