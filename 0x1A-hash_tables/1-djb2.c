10090b623b6ed612aff1b2f7478c274b707f71de
GithinjiJustus <justusgithinji257@gmail.com>
2024-02-04 23:57:20 -0800

    Smino

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,17 +1,9 @@
-#include "hash_tables.h"
-
-/**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash
- *
- * Description: Computes the index with the djb2 algorithm.
- * Return: The hash code
- */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
+    unsigned long int hash;
     int c;
 
+    hash = 5381;
     while ((c = *str++))
     {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
